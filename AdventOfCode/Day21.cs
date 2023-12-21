0924ba9a3b9c46c4841e7278ee2e35999cebebef
Artuur-Oerlemans <artuur.oerlemans@gmail.com>
2023-12-21 20:53:19 +0100

    day 21, top 100!!!!!!!!

--- AdventOfCode/Day21.cs
+++ AdventOfCode/Day21.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode;
 
@@ -28,10 +29,7 @@ public class Day21 : BaseDay
         {
             for (int j = 0; j < _input[i].Length; j++)
             {
-                if (_input[i][j] != '.')
-                {
-                    _dict.Add((i, j), _input[i][j]);
-                }
+                _dict.Add((i, j), _input[i][j]);
             }
         }
 
@@ -39,12 +37,131 @@ public class Day21 : BaseDay
     public override ValueTask<string> Solve_1()
     {
         long result = 0;
-        return new ValueTask<string>($"Solution to {ClassPrefix} {result}, part 1");
-    }
+        var start = _dict.Where(x => x.Value == 'S').Select(x => x.Key).Single();
 
+        HashSet<(long, long)> plotsNow = [];
+        plotsNow.Add(start);
+        for (int step = 0; step < 64; step++)
+        {
+            HashSet<(long, long)> plotsFuture = [];
+            foreach(var p in plotsNow)
+            {
+                foreach(var dir in dirs)
+                {
+                    var newP = Add(p, dir);
+                    if(_dict.ContainsKey(newP) && _dict[newP] != '#')
+                    {
+                        plotsFuture.Add(newP);
+                    }
+                }
+            }
+            plotsNow = plotsFuture;
+        }
+        return new ValueTask<string>($"Solution to {ClassPrefix} {plotsNow.Count}, part 1");
+    }
     public override ValueTask<string> Solve_2()
     {
         long result = 0;
+        var start = _dict.Where(x => x.Value == 'S').Select(x => x.Key).Single();
+
+        HashSet<(long, long)> plotsNow = [];
+        List<long> plotCounts = [0];
+        plotsNow.Add(start);
+        long goalTot = 16733044;
+        long sampleSteps = (goalTot % maxI) + maxI * 3;
+        for (int step = 0; step < sampleSteps; step++)
+        {
+            HashSet<(long, long)> plotsFuture = [];
+            foreach (var p in plotsNow)
+            {
+                foreach (var dir in dirs)
+                {
+                    var newP = Add(p, dir);
+                    var modP = Mod(newP);
+                    if (_dict[modP] != '#')
+                    {
+                        plotsFuture.Add(newP);
+                    }
+                }
+            }
+            plotsNow = plotsFuture;
+            L(step + " " + plotsNow.Count);
+            plotCounts.Add(plotsNow.Count);
+        }
+
+        long stepC = sampleSteps;
+        long plots = plotCounts[(int)stepC];
+
+        long defaultIncrease = plots - plotCounts[(int)(stepC - maxI)];
+        long defaultIncreaseMin = plotCounts[(int)(stepC - maxI)] - plotCounts[(int)(stepC - 2 * maxI)];
+
+        long metaIncrease = defaultIncrease - defaultIncreaseMin;
+
+        int d = 1;
+        while(stepC < goalTot)
+        {
+            plots += defaultIncrease + metaIncrease * d;
+            d++;
+            stepC += maxI;
+        }
+
+        return new ValueTask<string>($"Solution to {ClassPrefix} {plots}, part 2");
+    }
+
+    public ValueTask<string> Solve_2_old()
+    {
+        long result = 0;
+        var start = _dict.Where(x => x.Value == 'S').Select(x => x.Key).Single();
+
+        Dictionary<(long, long), HashSet<(long, long)>> plotsNow = [];
+        plotsNow.Add(start, new HashSet<(long, long)>{ (0, 0) });
+        for (int step = 0; step < 10; step++)
+        {
+            Dictionary<(long, long), HashSet<(long, long)>> plotsFuture = [];
+            foreach (var p in plotsNow)
+            {
+                foreach (var dir in dirs)
+                {
+                    var newP = Add(p.Key, dir);
+                    var modP = Mod(newP);
+                    var multi = p.Value;
+                    if (_dict[modP] != '#')
+                    {
+                        if (!_dict.ContainsKey(newP))
+                        {
+                            if(newP.Item1 >= maxI)
+                            {
+                                multi = multi.Select(x => (x.Item1 + 1, x.Item2)).ToHashSet();
+                            } else if (newP.Item2 >= maxJ)
+                            {
+                                multi = multi.Select(x => (x.Item1, x.Item2 + 1)).ToHashSet();
+                            } else if (newP.Item1 < 0)
+                            {
+                                multi = multi.Select(x => (x.Item1 - 1, x.Item2)).ToHashSet();
+                            } else if (newP.Item2 < 0)
+                            {
+                                multi = multi.Select(x => (x.Item1, x.Item2 - 1)).ToHashSet();
+                            } else
+                            {
+                                throw new Exception();
+                            }
+                        }
+
+                        if (plotsFuture.ContainsKey(modP))
+                        {
+                            plotsFuture[modP].UnionWith(multi);
+                        }
+                        else
+                        {
+                            plotsFuture[modP] = multi.ToHashSet();
+                        }
+                    }
+                }
+            }
+            plotsNow = plotsFuture;
+        }
+
+        result = plotsNow.Select(x => (long)x.Value.Count).Sum();
         return new ValueTask<string>($"Solution to {ClassPrefix} {result}, part 2");
     }
 
@@ -126,5 +243,10 @@ move 3 to C";
     private (long, long) Neg((long, long) p)
     {
         return (-p.Item1, -p.Item2);
+    }
+
+    private (long, long) Mod((long, long) p)
+    {
+        return ((p.Item1 % maxI + maxI) % maxI, (p.Item2 % maxJ + maxJ) % maxJ);
     }
 }
