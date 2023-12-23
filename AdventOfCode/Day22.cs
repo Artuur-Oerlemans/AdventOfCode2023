461b499afc23804ce032dd9d4e52d3ca802286fe
Artuur-Oerlemans <artuur.oerlemans@gmail.com>
2023-12-23 08:30:26 +0100

    day 22 & day 23

---
+++ AdventOfCode/Day22.cs
@@ -0,0 +1,240 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+public class Day22 : BaseDay
+{
+    private readonly List<string> _input;
+    private readonly Dictionary<(long, long), char> _dict;
+    private readonly List<(long, long)> dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    private readonly long maxI;
+    private readonly long maxJ;
+
+    public Day22()
+    {
+        _input =
+        [
+            .. File.ReadAllText(string.Concat(InputFilePath.AsSpan(0, InputFilePath.Length - 4)
+//, "test"
+, ".txt")
+                        ).Split("\r\n")
+,
+        ];
+
+        maxI = _input.Count;
+        maxJ = _input[0].Length;
+        _dict = [];
+        for (int i = 0; i < _input.Count; i++)
+        {
+            for (int j = 0; j < _input[i].Length; j++)
+            {
+                if (_input[i][j] != '.')
+                {
+                    _dict.Add((i, j), _input[i][j]);
+                }
+            }
+        }
+
+    }
+    public override ValueTask<string> Solve_1()
+    {
+        long result = 0;
+        var initialCoor = _input.Select(x => x.Split("~"))
+            .Select(x => (ToCoor(x[0]), ToCoor(x[1])))
+            .OrderBy( x => x.Item1.Item3)
+            .ToList();
+        Dictionary<int, List<int>> leansOn = [];
+
+        for(int s = 0; s < initialCoor.Count; s++)
+        {
+            var brick = initialCoor[s];
+            var newBrick = (Add(brick.Item1, (0, 0, -1)), Add(brick.Item2, (0, 0, -1)));
+            var inter = Intersects(initialCoor, s, newBrick);
+            while (newBrick.Item1.Item3 > 0 && inter.Count == 0)
+            {
+                brick = newBrick;
+                newBrick = (Add(brick.Item1, (0, 0, -1)), Add(brick.Item2, (0, 0, -1)));
+                inter = Intersects(initialCoor, s, newBrick);
+            }
+
+            initialCoor[s] = brick;
+            leansOn[s] = inter;
+        }
+
+        var criticiallyLeanOn = leansOn.Values.Where(x => x.Count == 1).Select(x => x[0]).ToHashSet();
+
+        for (int s = 0; s < initialCoor.Count; s++)
+        {
+            if(!criticiallyLeanOn.Contains(s))
+            {
+                result++;
+            }
+        }
+
+        return new ValueTask<string>($"Solution to {ClassPrefix} {result}, part 1");
+    }
+
+    List<int> Intersects(List<((long, long, long), (long, long, long))> initialCoor, int s, ((long, long, long), (long, long, long)) newBrick)
+    {
+        var inter = new  List<int>();
+        for(int d = 0; d < s; d++)
+        {
+            var otherBrick = initialCoor[d];
+
+            if(isOverlap(newBrick, otherBrick))
+            {
+                inter.Add(d);
+            }
+        }
+        return inter;
+
+    }
+
+    bool isOverlap(((long, long, long) s, (long, long, long) e) a, ((long, long, long) s, (long, long, long) e) b)
+    {
+        return a.s.Item1 <= b.e.Item1 && b.s.Item1 <= a.e.Item1
+            && a.s.Item2 <= b.e.Item2 && b.s.Item2 <= a.e.Item2
+            && a.s.Item3 <= b.e.Item3 && b.s.Item3 <= a.e.Item3;
+    }
+
+    (long, long, long) ToCoor(string input)
+    {
+        var t = input.Split(",").Select(x => long.Parse(x)).ToList(); ;
+
+        return (t[0], t[1], t[2]);
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        long result = 0;
+        var initialCoor = _input.Select(x => x.Split("~"))
+            .Select(x => (ToCoor(x[0]), ToCoor(x[1])))
+            .OrderBy(x => x.Item1.Item3)
+            .ToList();
+        Dictionary<int, HashSet<int>> leansOn = [];
+
+        for (int s = 0; s < initialCoor.Count; s++)
+        {
+            var brick = initialCoor[s];
+            var newBrick = (Add(brick.Item1, (0, 0, -1)), Add(brick.Item2, (0, 0, -1)));
+            var inter = Intersects(initialCoor, s, newBrick);
+            while (newBrick.Item1.Item3 > 0 && inter.Count == 0)
+            {
+                brick = newBrick;
+                newBrick = (Add(brick.Item1, (0, 0, -1)), Add(brick.Item2, (0, 0, -1)));
+                inter = Intersects(initialCoor, s, newBrick);
+            }
+
+            initialCoor[s] = brick;
+            leansOn[s] = inter.ToHashSet();
+        }
+
+        for (int s = 0; s < initialCoor.Count; s++)
+        {
+            HashSet<int> bricks = new HashSet<int>();
+            bricks.Add(s);
+            bool added = true;
+            while (added)
+            {
+                added = false;
+                for (int d = 0; d < initialCoor.Count; d++)
+                {
+                    if (!bricks.Contains(d) && leansOn[d].Count > 0 && leansOn[d].IsSubsetOf(bricks))
+                    {
+                        bricks.Add(d);
+                        added = true;
+                    }
+                }
+            }
+
+            result += bricks.Count - 1;
+        }
+
+        return new ValueTask<string>($"Solution to {ClassPrefix} {result}, part 2");
+    }
+
+    private string DictToString(Dictionary<(int, int), char> dict)
+    {
+        string r = "";
+        for (int i = 0; i < maxI; i++)
+        {
+            for (int j = 0; j < maxJ; j++)
+            {
+                if (dict.ContainsKey((i, j)))
+                {
+                    r += dict[(i, j)];
+                }
+                else
+                {
+                    r += '.';
+                }
+            }
+            r += "\r\n";
+        }
+        return r;
+    }
+
+    private String LocationSetToString(HashSet<(int, int)> energized)
+    {
+        string r = "";
+        for (int i = 0; i < maxI; i++)
+        {
+            for (int j = 0; j < maxJ; j++)
+            {
+                if (energized.Contains((i, j)))
+                {
+                    r += "#";
+                }
+                else
+                {
+                    r += '.';
+                }
+            }
+            r += "\r\n";
+        }
+        return r;
+    }
+
+    private void L(Object o)
+    {
+        Console.WriteLine(o.ToString());
+    }
+
+    private void RegexYouKnow()
+    {
+        string temps =
+@"move 1 to A
+move 2 to B
+move 3 to C";
+        Regex re = new(@"^move (\d) to ([A-Z])$");
+        temps.Split("\r\n")
+            .Select(s => re.Match(s).Groups)
+            .Select(g => (int.Parse(g[1].Value), g[2].Value))
+            .ToList().ForEach(t => Console.WriteLine(t.Item2));
+    }
+
+    private (long, long) Add((long, long) a, (long, long) b)
+    {
+        return (a.Item1 + b.Item1, a.Item2 + b.Item2);
+    }
+
+    private (long, long, long) Add((long, long, long) a, (long, long, long) b)
+    {
+        return (a.Item1 + b.Item1, a.Item2 + b.Item2, a.Item3 + b.Item3);
+    }
+
+    private (long, long) Mul((long, long) a, long x)
+    {
+        return (a.Item1 * x, a.Item2 * x);
+    }
+
+    private long ManDis((long, long) a, (long, long) b)
+    {
+        return Math.Abs(a.Item1 - a.Item1) + Math.Abs(b.Item2 - b.Item2);
+    }
+
+    private (long, long) Neg((long, long) p)
+    {
+        return (-p.Item1, -p.Item2);
+    }
+}
---
+++ AdventOfCode/Day23.cs
@@ -0,0 +1,291 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+public class Day23 : BaseDay
+{
+    private readonly List<string> _input;
+    private readonly Dictionary<(long, long), char> _dict;
+    private readonly List<(long, long)> dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    private readonly long maxI;
+    private readonly long maxJ;
+
+    public Day23()
+    {
+        _input =
+        [
+            .. File.ReadAllText(string.Concat(InputFilePath.AsSpan(0, InputFilePath.Length - 4)
+//, "test"
+, ".txt")
+                        ).Split("\r\n")
+,
+        ];
+
+        maxI = _input.Count;
+        maxJ = _input[0].Length;
+        _dict = [];
+        for (int i = 0; i < _input.Count; i++)
+        {
+            for (int j = 0; j < _input[i].Length; j++)
+            {
+                _dict.Add((i, j), _input[i][j]);
+            }
+        }
+
+    }
+    public override ValueTask<string> Solve_1()
+    {
+        long result = 0;
+
+        Stack<((long, long), HashSet<(long, long)>)> que = [];
+        que.Push(((0, 1), [(0, 1)]));
+
+        while (que.Count > 0)
+        {
+            ((long, long) loc, HashSet<(long, long)> history) = que.Pop();
+            List<(long, long)> locDirs;
+
+            if (loc == (maxI - 1, maxJ - 2))
+            {
+                result = Math.Max(history.Count - 1, result);
+            }
+
+            if (_dict[loc] == '<')
+            {
+                locDirs = [(0, -1)];
+            }
+            else if (_dict[loc] == '^')
+            {
+                locDirs = [(-1, 0)];
+            }
+            else if (_dict[loc] == '>')
+            {
+                locDirs = [(0, 1)];
+            }
+            else if (_dict[loc] == 'v')
+            {
+                locDirs = [(1, 0)];
+            }
+            else
+            {
+                locDirs = dirs;
+            }
+
+
+            foreach (var dir in locDirs)
+            {
+                var newLoc = Add(loc, dir);
+
+                if (_dict.ContainsKey(newLoc) && _dict[newLoc] != '#' && !history.Contains(newLoc))
+                {
+                    var newHistory = history.ToHashSet();
+                    newHistory.Add(newLoc);
+                    que.Push((newLoc, newHistory));
+                }
+            }
+        }
+
+        return new ValueTask<string>($"Solution to {ClassPrefix} {result}, part 1");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        long result = 0;
+
+        var graph = new Dictionary<(long, long), Dictionary<(long, long), long>>
+        {
+            { (0, 1), new Dictionary<(long, long), long>() }
+        };
+
+        Queue<((long, long), List<(long, long)>, (long, long), long)> que = [];
+        que.Enqueue(((0, 1), [(0, 1)], (0, 1), 0));
+
+        while (que.Count > 0)
+        {
+            ((long, long) loc, List<(long, long)> history, var lastKnot, var lastKnotDis) = que.Dequeue();
+
+            List<((long, long), List<(long, long)>)> nextRoutes = [];
+            foreach (var dir in dirs)
+            {
+                var newLoc = Add(loc, dir);
+
+                if (_dict.ContainsKey(newLoc) && _dict[newLoc] != '#' && (history.Count == 1 || history[^2] != newLoc))
+                {
+                    var newHistory = history.ToList();
+                    newHistory.Add(newLoc);
+                    nextRoutes.Add((newLoc, newHistory));
+                }
+            }
+
+            if (loc == (maxI - 1, maxJ - 2))
+            {
+                graph[lastKnot].Add((maxI - 1, maxJ - 2), lastKnotDis);
+                continue;
+            } else if (nextRoutes.Count > 1)
+            {
+                if (!graph.ContainsKey(loc)) { 
+                    graph[loc] = new Dictionary<(long, long), long>();
+                    foreach (var route in nextRoutes)
+                    {
+                        que.Enqueue((route.Item1, route.Item2, loc, 1));
+                    }
+                } 
+
+                if (!graph[lastKnot].ContainsKey(loc) || graph[lastKnot][loc] < lastKnotDis)
+                {
+                    graph[lastKnot][loc] = lastKnotDis;
+                    graph[loc][lastKnot] = lastKnotDis;
+                }
+            }
+            if (nextRoutes.Count == 1)
+            {
+                que.Enqueue((nextRoutes[0].Item1, nextRoutes[0].Item2, lastKnot, lastKnotDis + 1));
+            }
+        }
+
+        Stack<((long, long), HashSet<(long, long)>, long)> paths = [];
+        paths.Push(((0, 1), [(0, 1)], 0));
+
+        while(paths.Count > 0)
+        {
+            ((long, long) loc, HashSet<(long, long)> history, long dist) = paths.Pop();
+            if(loc == (maxI - 1, maxJ - 2))
+            {
+                if(dist > result)
+                {
+                    result = dist;
+                    //L(dist + " remaining path: " + paths.Count );
+                }
+                continue;
+            }
+
+            foreach ((long, long) newLoc in graph[loc].Keys)
+            {
+                if(!history.Contains(newLoc))
+                {
+                    var newHistory = history.ToHashSet();
+                    newHistory.Add(newLoc);
+                    paths.Push((newLoc, newHistory, dist + graph[loc][newLoc]));
+                }
+            }
+        }
+
+
+        return new ValueTask<string>($"Solution to {ClassPrefix} {result}, part 2");
+    }
+
+    public ValueTask<string> Solve_2_old()
+    {
+        long result = 0;
+
+        Queue<((long, long), HashSet<(long, long)>)> que = [];
+        que.Enqueue(((0, 1), [(0, 1)]));
+
+        while (que.Count > 0)
+        {
+            ((long, long) loc, HashSet<(long, long)> history) = que.Dequeue();
+            List<(long, long)> locDirs;
+
+            if (loc == (maxI - 1, maxJ - 2))
+            {
+                result = Math.Max(history.Count - 1, result);
+            }
+
+            locDirs = dirs;
+
+
+            foreach (var dir in locDirs)
+            {
+                var newLoc = Add(loc, dir);
+
+                if (_dict.ContainsKey(newLoc) && _dict[newLoc] != '#' && !history.Contains(newLoc))
+                {
+                    var newHistory = history.ToHashSet();
+                    newHistory.Add(newLoc);
+                    que.Enqueue((newLoc, newHistory));
+                }
+            }
+        }
+        return new ValueTask<string>($"Solution to {ClassPrefix} {result}, part 2");
+    }
+
+    private string DictToString(Dictionary<(int, int), char> dict)
+    {
+        string r = "";
+        for (int i = 0; i < maxI; i++)
+        {
+            for (int j = 0; j < maxJ; j++)
+            {
+                if (dict.ContainsKey((i, j)))
+                {
+                    r += dict[(i, j)];
+                }
+                else
+                {
+                    r += '.';
+                }
+            }
+            r += "\r\n";
+        }
+        return r;
+    }
+
+    private String LocationSetToString(HashSet<(int, int)> energized)
+    {
+        string r = "";
+        for (int i = 0; i < maxI; i++)
+        {
+            for (int j = 0; j < maxJ; j++)
+            {
+                if (energized.Contains((i, j)))
+                {
+                    r += "#";
+                }
+                else
+                {
+                    r += '.';
+                }
+            }
+            r += "\r\n";
+        }
+        return r;
+    }
+
+    private void L(Object o)
+    {
+        Console.WriteLine(o.ToString());
+    }
+
+    private void RegexYouKnow()
+    {
+        string temps =
+@"move 1 to A
+move 2 to B
+move 3 to C";
+        Regex re = new(@"^move (\d) to ([A-Z])$");
+        temps.Split("\r\n")
+            .Select(s => re.Match(s).Groups)
+            .Select(g => (int.Parse(g[1].Value), g[2].Value))
+            .ToList().ForEach(t => Console.WriteLine(t.Item2));
+    }
+
+    private (long, long) Add((long, long) a, (long, long) b)
+    {
+        return (a.Item1 + b.Item1, a.Item2 + b.Item2);
+    }
+
+    private (long, long) Mul((long, long) a, long x)
+    {
+        return (a.Item1 * x, a.Item2 * x);
+    }
+
+    private long ManDis((long, long) a, (long, long) b)
+    {
+        return Math.Abs(a.Item1 - a.Item1) + Math.Abs(b.Item2 - b.Item2);
+    }
+
+    private (long, long) Neg((long, long) p)
+    {
+        return (-p.Item1, -p.Item2);
+    }
+}
